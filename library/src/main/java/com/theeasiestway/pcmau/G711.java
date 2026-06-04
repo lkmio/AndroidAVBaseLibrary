5b1bed96aa9223289aaca0632bbe181feb8c49c7
ydajiang <diyidacheng@gmail.com>
2026-06-04 15:50:44 +0800

    feat: 更新G711库, 支持directbuffer;

--- library/src/main/java/com/theeasiestway/pcmau/G711.java
+++ library/src/main/java/com/theeasiestway/pcmau/G711.java
@@ -18,6 +18,12 @@ public class G711 {
 
     public static native byte[] encodeU(byte[] bytes);
 
+    public static native void encodeU(byte[] pcm, int offset, int length, byte[] g711u, int g711u_offset);
+
+    public static native void encodeADirect(java.nio.ByteBuffer pcm, java.nio.ByteBuffer g711a);
+
+    public static native void encodeUDirect(java.nio.ByteBuffer pcm, java.nio.ByteBuffer g711u);
+
     public static native short[] encodeU(short[] shorts);
 
     //
@@ -31,6 +37,12 @@ public class G711 {
     public static native short[] decodeA(short[] shorts);
 
     public static native byte[] decodeU(byte[] bytes);
+
+    public static native void decodeU(byte[] g711u, int offset, int length, byte[] pcm, int pcm_offset);
+
+    public static native void decodeADirect(java.nio.ByteBuffer g711a, java.nio.ByteBuffer pcm);
+
+    public static native void decodeUDirect(java.nio.ByteBuffer g711u, java.nio.ByteBuffer pcm);
 
     public static native short[] decodeU(short[] shorts);
 
